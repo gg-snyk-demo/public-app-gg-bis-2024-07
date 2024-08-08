c180a801a09c593854f43e675c0c33da804471d4
Bruce Banner <bruce-banner-snyk@protonmail.com>
2024-08-08 22:03:52 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import antigravity
 
-apikey=key-7c33e58c77ecab8c7895c637ec00d0c1
+CERT="""-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACCLT3M9fkuknETTNwMmlkxoXqwmontYNtGwi06Yak1GeAAAAJCQR0T6kEdE
++gAAAAtzc2gtZWQyNTUxOQAAACCLT3M9fkuknETTNwMmlkxoXqwmontYNtGwi06Yak1GeA
+AAAEBLFe2P7DoxXAsk8OK0ibLl0KbzkWqXVhOq7twodZivHYtPcz1+S6ScRNM3AyaWTGhe
+rCaie1g20bCLTphqTUZ4AAAABm15LWtleQECAwQFBgc=
+-----END OPENSSH PRIVATE KEY-----
+"""
 
 def main():
     antigravity.fly()
